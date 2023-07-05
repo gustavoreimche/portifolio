5b57a9be1618ba7a7f21a83ad00c12b5c1abb57f
Gustavo Reimche <gustavoreimche@hotmail.com>
2023-07-05 11:16:58 -0300

    update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'portifolio';
+  @ViewChild('contactSection', { static: true, read: ElementRef })
+  contactSection!: ElementRef;
+
+  scrollToContact(): void {
+    console.log('Scrolling to contact');
+    this.contactSection.nativeElement.scrollIntoView({ behavior: 'smooth' });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,60 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './views/template/header/header.component';
+import { FooterComponent } from './views/template/footer/footer.component';
+import {
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+} from '@angular/platform-browser/animations';
+import { LogoComponent } from './components/logo/logo.component';
+import { ContactMeButtonComponent } from './components/contact-me-button/contact-me-button.component';
+import { HomeComponent } from './views/home/home.component';
+import { NgxTypedJsModule } from 'ngx-typed-js';
+import { ContactComponent } from './views/contact/contact.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LoadingComponent } from './components/loading/loading.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { HttpClientModule } from '@angular/common/http';
+import { ExperienceComponent } from './views/experience/experience.component';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { SkillsComponent } from './views/skills/skills.component';
+import { CoursesComponent } from './views/courses/courses.component';
+import { ProjectsComponent } from './views/projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LogoComponent,
+    ContactMeButtonComponent,
+    HomeComponent,
+    ContactComponent,
+    LoadingComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    CoursesComponent,
+    ProjectsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    NgxTypedJsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    HttpClientModule,
+    MatProgressBarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
