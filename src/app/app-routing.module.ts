1a854e75f8390461d00591e9bb284c795f083434
Gustavo Reimche <gustavoreimche@hotmail.com>
2023-07-05 11:03:47 -0300

    update routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { ExperienceComponent } from './views/experience/experience.component';
+import { SkillsComponent } from './views/skills/skills.component';
+import { CoursesComponent } from './views/courses/courses.component';
+import { ProjectsComponent } from './views/projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'experiences',
+    component: ExperienceComponent,
+  },
+  {
+    path: 'skills',
+    component: SkillsComponent,
+  },
+  {
+    path: 'courses',
+    component: CoursesComponent,
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent,
+  },
+  {
+    path: '*',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
